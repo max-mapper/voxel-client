a0582d25bc41876f977577aa79caf24f04d57895
Max Ogden <max@maxogden.com>
2013-04-11 14:32:26 -0700

    fix start script

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ Client.prototype.bindEvents = function(socket) {
   })
   
   emitter.on('settings', function(settings) {
-	settings.texturePath = texturePath
+    settings.texturePath = texturePath
     settings.generateChunks = false
     self.game = self.createGame(settings)
     emitter.emit('created')
@@ -61,12 +61,6 @@ Client.prototype.bindEvents = function(socket) {
       chunk.voxels = voxels
       self.game.showChunk(chunk)
     })
-    // emitter.on('noMoreChunks', function() {
-//       var createPlayer = player(self.game)
-//       self.viking = createPlayer('viking.png')
-//       if (settings.startingPosition) self.viking.moveTo(settings.startingPosition)
-//       self.viking.possess()
-//     })
   })
 
   // fires when server sends us voxel edits
@@ -109,37 +103,6 @@ Client.prototype.createGame = function(options) {
     
   emitChat(name, emitter)
 
-  // var container = document.body
-//   self.game.appendTo(container)
-  // rescue(game)
-  
-  
-  //highlight(self.game)
-  
-  // blockSelector.on('select', function(material) {
-  //   currentMaterial = +material
-  // })
-  
-  // self.game.on('fire', function (target, state) {
-  //   var vec = self.game.cameraVector()
-  //   var pos = self.game.cameraPosition()
-  //   var point = self.game.raycast(pos, vec, 100)
-  //   if (!point) return
-  //   var erase = !state.firealt && !state.alt
-  //   var size = self.game.cubeSize
-  //   if (erase) {
-  //     emitter.emit('set', {x: point.x, y: point.y, z: point.z}, 0)
-  //   } else {
-  //     var newBlock = self.game.checkBlock(point)
-  //     if (!newBlock) return
-  //     var direction = self.game.camera.matrixWorld.multiplyVector3(new self.game.THREE.Vector3(0,0,-1))
-  //     var diff = direction.subSelf(self.game.controls.target().yaw.position.clone()).normalize()
-  //     diff.multiplySelf({ x: 1, y: 1, z: 1 })
-  //     var p = point.clone().addSelf(diff)
-  //     emitter.emit('set', p, currentMaterial)
-  //   }
-  // })
-  
   // setTimeout is because three.js seems to throw errors if you add stuff too soon
   setTimeout(function() {
     emitter.on('update', function(updates) {      
@@ -161,8 +124,6 @@ Client.prototype.createGame = function(options) {
 }
 
 Client.prototype.onServerUpdate = function(update) {
-  var pos = this.game.controls.target().yaw.position
-  var distance = pos.distanceTo(update.position)
   // todo use server sent location
 }
 
