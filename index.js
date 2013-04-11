f247da9eb0e62c8e9879d7e2e1518b488d4d7ed4
Chris Kelley <chris@vetula.com>
2013-04-11 17:34:05 +0200

    Updated packages; resolved some scope issues with game

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var emitChat = require('./chat')
 var highlight = require('voxel-highlight')
 var skin = require('minecraft-skin')
 var player = require('voxel-player')
+var texturePath = "/textures/"
+//var game
 
 module.exports = Client
 
@@ -26,6 +28,7 @@ function Client(server) {
   this.server = server || 'ws://' + url.parse(window.location.href).host
   this.others = {}
   this.connect(server)
+  this.game
   window.others = this.others
 }
 
@@ -49,25 +52,26 @@ Client.prototype.bindEvents = function(socket) {
   })
   
   emitter.on('settings', function(settings) {
+	settings.texturePath = texturePath
     settings.generateChunks = false
     self.game = self.createGame(settings)
     emitter.emit('created')
     emitter.on('chunk', function(encoded, chunk) {
       var voxels = crunch.decode(encoded, chunk.length)
       chunk.voxels = voxels
-      game.showChunk(chunk)
+      self.game.showChunk(chunk)
     })
-    emitter.on('noMoreChunks', function() {
-      var createPlayer = player(self.game)
-      var viking = createPlayer('viking.png')
-      if (settings.startingPosition) viking.moveTo(settings.startingPosition)
-      viking.possess()
-    })
+    // emitter.on('noMoreChunks', function() {
+//       var createPlayer = player(self.game)
+//       self.viking = createPlayer('viking.png')
+//       if (settings.startingPosition) self.viking.moveTo(settings.startingPosition)
+//       self.viking.possess()
+//     })
   })
 
   // fires when server sends us voxel edits
   emitter.on('set', function(pos, val) {
-    game.setBlock(pos, val)
+    self.game.setBlock(pos, val)
   })
 }
 
@@ -75,10 +79,10 @@ Client.prototype.createGame = function(options) {
   var self = this
   var emitter = this.emitter
   options.controlsDisabled = false
-  window.game = engine(options)
+  window.game = self.game = engine(options)
   function sendState() {
     if (!self.connected) return
-    var player = game.controls.target()
+    var player = self.game.controls.target()
     var state = {
       position: player.yaw.position,
       rotation: {
@@ -95,7 +99,7 @@ Client.prototype.createGame = function(options) {
     localStorage.setItem('name', name)
   }
 
-  game.controls.on('data', function(state) {
+  self.game.controls.on('data', function(state) {
     var interacting = false
     Object.keys(state).map(function(control) {
       if (state[control] > 0) interacting = true
@@ -105,36 +109,36 @@ Client.prototype.createGame = function(options) {
     
   emitChat(name, emitter)
 
-  var container = document.body
-  game.appendTo(container)
+  // var container = document.body
+//   self.game.appendTo(container)
   // rescue(game)
   
   
-  highlight(game)
+  //highlight(self.game)
   
   // blockSelector.on('select', function(material) {
   //   currentMaterial = +material
   // })
   
-  game.on('fire', function (target, state) {
-    var vec = game.cameraVector()
-    var pos = game.cameraPosition()
-    var point = game.raycast(pos, vec, 100)
-    if (!point) return
-    var erase = !state.firealt && !state.alt
-    var size = game.cubeSize
-    if (erase) {
-      emitter.emit('set', {x: point.x, y: point.y, z: point.z}, 0)
-    } else {
-      var newBlock = game.checkBlock(point)
-      if (!newBlock) return
-      var direction = game.camera.matrixWorld.multiplyVector3(new game.THREE.Vector3(0,0,-1))
-      var diff = direction.subSelf(game.controls.target().yaw.position.clone()).normalize()
-      diff.multiplySelf({ x: 1, y: 1, z: 1 })
-      var p = point.clone().addSelf(diff)
-      emitter.emit('set', p, currentMaterial)
-    }
-  })
+  // self.game.on('fire', function (target, state) {
+  //   var vec = self.game.cameraVector()
+  //   var pos = self.game.cameraPosition()
+  //   var point = self.game.raycast(pos, vec, 100)
+  //   if (!point) return
+  //   var erase = !state.firealt && !state.alt
+  //   var size = self.game.cubeSize
+  //   if (erase) {
+  //     emitter.emit('set', {x: point.x, y: point.y, z: point.z}, 0)
+  //   } else {
+  //     var newBlock = self.game.checkBlock(point)
+  //     if (!newBlock) return
+  //     var direction = self.game.camera.matrixWorld.multiplyVector3(new self.game.THREE.Vector3(0,0,-1))
+  //     var diff = direction.subSelf(self.game.controls.target().yaw.position.clone()).normalize()
+  //     diff.multiplySelf({ x: 1, y: 1, z: 1 })
+  //     var p = point.clone().addSelf(diff)
+  //     emitter.emit('set', p, currentMaterial)
+  //   }
+  // })
   
   // setTimeout is because three.js seems to throw errors if you add stuff too soon
   setTimeout(function() {
@@ -149,11 +153,11 @@ Client.prototype.createGame = function(options) {
 
   emitter.on('leave', function(id) {
     if (!self.others[id]) return
-    game.scene.remove(self.others[id].mesh)
+    self.game.scene.remove(self.others[id].mesh)
     delete self.others[id]
   })
   
-  return game
+  return self.game
 }
 
 Client.prototype.onServerUpdate = function(update) {
@@ -173,8 +177,8 @@ Client.prototype.updatePlayerPosition = function(id, update) {
   var pos = update.position
   var player = this.others[id]
   if (!player) {
-    var playerSkin = skin(game.THREE, 'viking.png', {
-      scale: new game.THREE.Vector3(0.04, 0.04, 0.04)
+    var playerSkin = skin(this.game.THREE, 'player.png', {
+      scale: new this.game.THREE.Vector3(0.04, 0.04, 0.04)
     })
     var playerMesh = playerSkin.mesh
     this.others[id] = playerSkin
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # voxel-client
 
-work in progress
+client for voxel-server
+
+# setup
+
+The file www/demo.js thorws errors; instead, follow the instructions in [voxel-hello-client](https://github.com/chrisekelley/voxel-hello-client) to get this running.
+
+BSD LICENSE
